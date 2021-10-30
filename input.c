30489d8b3787b2a8664681cc0da5f81fc2037322
Alejandro Madriñán <alejandro.madrinanfer@alumnos.upm.es>
2021-10-29 19:08:25 -0500

    input flow

--- input.c
+++ input.c
@@ -5,6 +5,14 @@ int main()
     int num;
 
     printf( "Please enter a number: " );
-    scanf( "%d", &num ); // the ampersand (&) operator gives memory direction
-    printf( "You entered %d", num );
+    scanf( "%d", &num ); // the ampersand (&) operator accesses memory direction
+    if ( num > 0 )
+        printf( "The number is positive.\n" );
+    else if ( num < 0 )
+        printf( "The number is negative.\n" );
+    else
+        printf( "The number is zero.\n" );
+    
+    printf( "You entered %d.", num );
+    
 }
